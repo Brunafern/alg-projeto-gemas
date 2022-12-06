96556a834240c8203e73ce1e1682da6a22f99d4e
Yonners <yonner_games@hotmail.com>
2022-12-06 09:16:10 -0300

    Primeiro Commit de zé

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from funcoes import *
+from funções import *
 
-linhas = int(input())
-colunas = int(input())
-cores = int(input())
+linha,coluna = map(int,input('Números de linha e coluna: ').split())
+
+quant_cores = int(input('Quantidades de cores: '))
+
+tabuleiro = CRIAR_TABULEIRO(linha,coluna)
+COMPLETAR_TABULEIRO(tabuleiro,quant_cores)
+PRINTAR_TABULEIRO(tabuleiro,coluna)
+
+CRIAR_TABULEIRO(int(input()),int(input()))
+
 
