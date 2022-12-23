45149fc57fd038cb0a7cdb9c79b6a9520043ca49
Yonners <yonner_games@hotmail.com>
2022-12-23 10:56:52 -0300

    toques finais_2

--- funcoes.py
+++ funcoes.py
@@ -67,6 +67,12 @@ def validar_troca_posicoes(posicoes1, tabu, posicoes_vezes ):
 
 
 def criar_tabuleiro(num_linhas,num_colunas):
+    '''
+
+    -param num_linhas: Número de linhas
+    -param num_colunas: Número de colunas
+    -return: Cria uma matriz (Tabuleiro)
+    '''
     tabuleiro = []
     for l in range(0,num_linhas):
         linha = []
@@ -79,35 +85,56 @@ def criar_tabuleiro(num_linhas,num_colunas):
 def inicar():
     print(JOGO_INICIO)
 
-def printar_tabuleiro(n,coluna):
+def printar_tabuleiro(tabu,coluna):
+    '''
+
+    -param tabu: Matriz (Tabuleiro)
+    -param coluna: Número de colunas
+    -return: Tabuleiro montado
+    '''
     quant_c = ['','','',''] # Ajusta os números superiores
     for i in range(0,coluna):
         quant_c.append(i)
-    for l in range(len(n)):
+    for l in range(len(tabu)):
         if l == 0:
             print(*quant_c,'\n'
                   '  +'+('--'*coluna)+'-+')
-        for c in range(len(n[l])):
+        for c in range(len(tabu[l])):
             if c == 0:
-                print(f'{l} | '+n[l][c], end=' ') # Números laterais
-            elif c == len(n[l]) -1:
-                print(n[l][c], end=' |')
+                print(f'{l} | '+tabu[l][c], end=' ') # Números laterais
+            elif c == len(tabu[l]) -1:
+                print(tabu[l][c], end=' |')
             else:
-                print(n[l][c], end=' ')
+                print(tabu[l][c], end=' ')
         print()
-        if l == len(n) -1:
+        if l == len(tabu) -1:
             print('  +'+('--'*coluna)+'-+')
 
-    return n
+    return tabu
 
-def completar_tabuleiro(t,cor):
-    for l in range(len(t)):
-        for c in range(len(t[l])):
-            if t[l][c] == VAZIO:
+def completar_tabuleiro(tabu,cor):
+    '''
+
+    -param tabu: Matriz (Tabuleiro)
+    -param cor: Números de cores a ser inserido no tabuleiro
+    -return: Tabuleiro com as gemas inseridas
+    '''
+    for l in range(len(tabu)):
+        for c in range(len(tabu[l])):
+            if tabu[l][c] == VAZIO:
                 r = random.randrange(0, cor) # Gera um indice aleatório para a lista cores
-                t[l][c] = CORES[r]
-    return t
+                tabu[l][c] = CORES[r]
+    return tabu
 def trocar_posicao(num_linha_1,num_coluna_1,num_linha_2,num_coluna_2,tabu):
+    '''
+
+    -param num_linha_1: Indice linha da gema um
+    -param num_coluna_1: Indice coluna da gema um
+    -param num_linha_2: Indice linha da gema dois
+    -param num_coluna_2: Indice coluna da gema dois
+    -param tabu: Matriz (Tabuleiro)
+    -return:
+    '''
     gema_1 = tabu[num_linha_1][num_coluna_1]
     gema_2 = tabu[num_linha_2][num_coluna_2]
     if (num_coluna_2 == num_coluna_1 and num_linha_2 == num_linha_1 + 1 or  num_linha_2 == num_linha_1 -1) or (num_linha_2 == num_linha_1 and num_coluna_2 == num_coluna_1 + 1 or  num_coluna_2 == num_coluna_1 -1):
@@ -120,6 +147,11 @@ def trocar_posicao(num_linha_1,num_coluna_1,num_linha_2,num_coluna_2,tabu):
         return False
 
 def identificar_gemas_linha(tabu):
+    '''
+
+    -param tabu: Matriz (Tabuleiro)
+    -return: Indices da cadeia de gemas a serem eliminadas na linha
+    '''
     indice_remove_l = []
     for l in range(len(tabu)):
         linha = []
@@ -153,6 +185,11 @@ def identificar_gemas_linha(tabu):
 
 
 def identificar_gemas_colunas(tabu):
+    '''
+
+    -param tabu: Matriz (Tabuleiro)
+    -return: Indices da cadeia de gemas a serem eliminadas na coluna
+    '''
     indice_remove_c = []
     for l in range(len(tabu)):
         linha = []
@@ -182,6 +219,13 @@ def identificar_gemas_colunas(tabu):
     return indice_remove_c
 
 def power_ups(linha,coluna,tabu):
+    '''
+
+    -param linha: Indices da função indentificar_gemas_linha
+    -param coluna: Indices da função indentificar_gemas_colunas
+    -param tabu: Matriz (Tabuleiro)
+    -return: Indices das gemas a serem eliminadas de acordo com o power ip especifico
+    '''
     p_5 = '0'
     indice_linha = []
     indice_coluna = []
@@ -198,7 +242,19 @@ def power_ups(linha,coluna,tabu):
 
     return indice_linha,indice_coluna,p_5
 
+
 def eliminar_gemas(l,c,tabu,p_l,p_c,p_5, pontos):
+    '''
+
+    -param l: Indices da função indentificar_gemas_linha
+    -param c: Indices da função indentificar_gemas_colunas
+    -param tabu: Matriz (Tabuleiro)
+    -param p_l: Indices linhas da função power_ups
+    -param p_c: Indices da colunas da função power_ups
+    -param p_5: Indices das gemas da função power_ups
+    -param pontos: soma das gemas eliminadas
+    -return: Tabuleiro e pontos
+    '''
     pontos += len(c) + len(l)
     for i in range(len(l)):
         tabu[l[i][0]][l[i][1]] = VAZIO
@@ -218,7 +274,13 @@ def eliminar_gemas(l,c,tabu,p_l,p_c,p_5, pontos):
                     tabu[l][c] = VAZIO
     return tabu, pontos
 
+
 def deslocar_gema(tabu):
+    '''
+
+    -param tabu: Matriz (Tabuleiro)
+    -return: Tabuleiro com as gemas caidas
+    '''
 
     for l in range(len(tabu)):
         indice_vazio = []
@@ -250,7 +312,12 @@ def deslocar_gema(tabu):
 
     return tabu
 
-def dicas(tl1, tc1 ,tabu, pontos):
+
+def dicas(tl1, tc1 , tabu, pontos):
+    #tl1 e tc1: verificam se a entrada é igual ao numero da dica
+    #tabu: verificar possiveis cadeias no tabuleiro
+    #pontos= se o usuario pedir uma dica, perde um ponto
+
    tem = VAZIO_SEM_ESPACO
    if tc1 == NUMERO_DICA and tl1 == NUMERO_DICA:
        pontos -= 1
@@ -298,5 +365,6 @@ def dicas(tl1, tc1 ,tabu, pontos):
            continuar = input(CONTINUAR)
            if continuar == NAO:
                 tem = NAO
+
    return tem, pontos
 
