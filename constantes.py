9833599adde2ea3c9aaac4624d19a5acac9877a7
Bruna Fernanda <fernanda.bruna@academico.ifpb.edu.br>
2022-12-26 13:11:52 -0300

    so os pontos

--- constantes.py
+++ constantes.py
@@ -20,10 +20,8 @@ MARGEM_TABULEIRO = 6
 NUMERO_DICA = 100
 ENTRADA_INVALIDA_INSIRA_NUMEROS = 'Entrada Inválida, insira numeros de 4 a 26'
 MENOR_ENTRADA_POSIVEL = 4
-JOGO_INICIO = '\n'"================================================="'\n' \
-                   "             Bem-vindo ao Gemas!                 "'\n' \
-                   "=================================================" \
-                   '\n''Para obter uma dica, digite 100 100''\n''Para trocar de posição, insira as cordenadas com um espaço no meio''\n'
+JOGO_INICIO = '\n'"================================================="'\n' "             Bem-vindo ao Gemas!                 "'\n' "=================================================" '\n''\n''-Tabuleiro de linhas e colunas iguais, os quais podem ser montados de 4x4 a 10x10. ''\n''-São escolhidos de 4 a 26 letras, no qual cada letra será uma gema.''\n''-As gemas são removidas caso sejam feitas cadeias de 3 ou mais gemas semelhantes.''\n''-Para deslocar as gemas é necessário que seja digitado os indices das gemas que iram trocar de posições.''\n''-Dicas podem ser obtidas ao digitar 100 100 no momento de escolher a posição da primeira gema a ser deslocada.''\n''-Power ups' 'podem ser feitos ao forma cadeias de 4 ou 5.''\n''-O jogo pode ser encerrado ao não ser apresentado nenhuma dica, porém ainda podem existir jogadas possiveis.''\n' '-Caso seja requerido uma dica, o jogadro ira perde um ponto.''\n' '-Para encerrar o jogo digite SAIR na posição um''\n'
+
 
 NUMERO_CORES = 'Números de Cores: '
 ERRO_INTERVALO = "Insira um numero de 4 a 10"
@@ -34,7 +32,16 @@ CORDENADA_FORA_TABLEIRO = "Esta cordenada não está presente no tabuleiro"
 INSIRA_NOVAMENTE_ = 'Insira novamente: '
 CONTINUAR = "Voce quer continuar?"
 
-POSICAO_DOIS = 'Posição dois: '
+POSICAO_DOIS ='Insira a cordenada da linha, espaço, coluna. ex. (x x)\n'\
+              'Posição dois: '
 
-POSICAO_UM = 'Posição um: '
+POSICAO_UM = 'Insira a cordenada da linha, espaço, coluna. ex. (x x)\n' \
+             'Posição um: '
+PONTOS_ = 'Pontos:'
 
+SAIR = 'SAIR'
+
+QUANTIDADES_DE_CORES_ = 'Quantidades de cores (4-26): '
+
+INSIRA_COLUNAS_LINHAS = 'Insira um número para linhas e colunas: '
+ENTRADA_DICA = '100 100'
